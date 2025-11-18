3caef4e1c6b570135b07f54cf4b64c02bd18c77c
이승률 <srlee8989@gmail.com>
2025-11-18 12:44:54 +0900

    add fan functionalities

--- src/components/VehicleArea.tsx
+++ src/components/VehicleArea.tsx
@@ -1,15 +1,41 @@
 import './VehicleArea.css';
+import { postFanStart, postFanClear, postFanAccel, postFanDeaccel } from '../utils/RestApi';
 
 interface VehicleAreaProps {}
 
 const VehicleArea = ({}: VehicleAreaProps) => {
+  const handleFanStart = async () => {
+    try {
+      await postFanStart();
+    } catch (e) {}
+  };
+  const handleFanClear = async () => {
+    try {
+      await postFanClear();
+    } catch (e) {}
+  };
+  const handleFanAccel = async () => {
+    try {
+      await postFanAccel();
+    } catch (e) {}
+  };
+  const handleFanDeaccel = async () => {
+    try {
+      await postFanDeaccel();
+    } catch (e) {}
+  };
+
   return (
     <div id="vehicle-area">
-      <div className="fan-area">
-        <div className="fan-inner-area"></div>
-      </div>
-      <div className="wheel-area">
-        <div className="wheel-inner-area"></div>
+      <div className="control-area">
+        <div className="fan-start" onClick={handleFanStart}></div>
+        <div className="fan-clear" onClick={handleFanClear}></div>
+        <div className="fan-deaccel" onClick={handleFanDeaccel}></div>
+        <div className="fan-accel" onClick={handleFanAccel}></div>
+        <div className="wheel-start"></div>
+        <div className="wheel-stop"></div>
+        <div className="wheela-auto"></div>
+        <div className="wheela-cali"></div>
       </div>
       <div className="mcu-less-1 mcu-status-green"></div>
       <div className="mcu-less-2 mcu-status-green"></div>
--- src/utils/RestApi.ts
+++ src/utils/RestApi.ts
@@ -25,3 +25,43 @@ export const getNetworkInfo = async () => {
     console.log('getNetwork API call completed');
   }
 };
+
+export const postFanStart = async () => {
+  try {
+    const response = await networkApi.post('/api/fan/start');
+    return response.data;
+  } catch (error) {
+    console.error('Error starting fan:', error);
+    throw error;
+  }
+};
+
+export const postFanClear = async () => {
+  try {
+    const response = await networkApi.post('/api/fan/clear');
+    return response.data;
+  } catch (error) {
+    console.error('Error clearing fan:', error);
+    throw error;
+  }
+};
+
+export const postFanAccel = async () => {
+  try {
+    const response = await networkApi.post('/api/fan/acceleration');
+    return response.data;
+  } catch (error) {
+    console.error('Error accelerating fan:', error);
+    throw error;
+  }
+};
+
+export const postFanDeaccel = async () => {
+  try {
+    const response = await networkApi.post('/api/fan/deacceleration');
+    return response.data;
+  } catch (error) {
+    console.error('Error deaccelerating fan:', error);
+    throw error;
+  }
+};
